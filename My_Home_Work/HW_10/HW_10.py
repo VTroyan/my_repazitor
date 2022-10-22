7153f2bf56a8821a4ed9bf1aa6b64ac9652bb85d
VTroyan <otaliktan@gmail.com>
2022-10-22 17:48:04 +0300

    update HW_10.py

--- My_Home_Work/HW_10/HW_10.py
+++ My_Home_Work/HW_10/HW_10.py
@@ -1,17 +1,35 @@
-class MyException:
-    print(
-        "Для завершения нажмите "
-        "Stop в качестве знака операции"
-    )
+class MyException(Exception):
+    def __init__(self, msg="Моя ошибка по умолчанию"):
+        self.msg = msg
+
+    def __str__(self): return self.msg
+
+
+print(
+    "Для завершения нажмите "
+    "Stop в качестве знака операции"
+)
+
+
+while True:
+    operation = input("Введите значение операции: ")
+    if operation == "stop":
+        break
+    elif operation not in ("+", "-", "*", "/"):
+        continue
+    # Цикл для ввода переменных, если возникает ошибка то цикл повторяеется
     while True:
-        operation = input("Введите значение операции: ")
-        if operation == "stop":
-            break
-        if operation not in ("+", "-", "*", "/"):
+        try:
+            num_1 = int(input("Введите первое число: "))
+
+        except ValueError:
             continue
 
-        num_1 = int(input("Введите первое число: "))
-        num_2 = int(input("Введите второе число: "))
+        try:
+            num_2 = int(input("Введите второе число: "))
+
+        except ValueError:
+            continue
 
         if operation == "+":
             print("Ответ: ", num_1 + num_2)
@@ -21,6 +39,9 @@ class MyException:
             print("Ответ: ", num_1 * num_2)
         elif operation == "/":
             try:
-                print(num_1/num_2)
-            except ZeroDivisionError as error:
-                print("Делить на ноль нельзя")
+                print(num_1 / num_2)
+            except ZeroDivisionError:
+                raise MyException()
+                # print("Делить на ноль нельзя")
+
+        break
